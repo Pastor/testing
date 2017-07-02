2165ee93cb90005db91eae18067d0f8bc3062b82
Андрей <viruszold@mail.ru>
2017-07-03 02:41:45 +0300

    Тестирование

--- rustlang/mini_restful_db/src/db.rs
+++ rustlang/mini_restful_db/src/db.rs
@@ -1,38 +1,98 @@
 extern crate rusqlite;
 extern crate time;
 
+extern crate serde;
+extern crate serde_json;
+
 use self::time::Timespec;
-use self::rusqlite::Connection;
+use self::rusqlite::{Connection, Result, Statement};
+use self::rusqlite::types::ToSql;
 use std::path::Path;
 use std::fs;
+use std::iter::Iterator;
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct Article {
-    id: i32,
-    title: String,
-    desc: String,
-    content: String,
-    created_at: Timespec,
-    // TODO: data: Option<Vec<u8>>
+    pub id: i32,
+    pub title: String,
+    pub desc: String,
+    pub content: String,
+    pub created_at: Timespec
 }
 
-pub fn init() {
-    let path = Path::new("sample.db");
-    fs::remove_file(path).unwrap();
-    let conn = Connection::open(path).unwrap();
+pub trait ToArticle {
+    fn to_article(&self) -> Article;
+}
 
-    conn.execute(
-        "CREATE TABLE articles(id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, title TEXT NOT NULL, desc TEXT NOT NULL, content TEXT NOT NULL)", &[]).unwrap();
+pub struct Service {
+    c: Connection
+}
 
-    let me = Article {
-        id: 0,
-        title: format!("Dan"),
-        desc: format!("Dan"),
-        content: format!("Dan"),
-        created_at: time::get_time(),
-    };
-    {
-        conn.execute("INSERT INTO articles(title, desc, content) VALUES(?1, ?2, ?3)",
-                     &[&me.title, &me.desc, &me.content]).unwrap();
+fn get_result(stmt: &mut Statement, params: &[&ToSql]) -> Vec<Article> {
+    let articles = stmt.query_map(params, |row| {
+        Article {
+            id: row.get(0),
+            title: row.get(1),
+            desc: row.get(2),
+            content: row.get(3),
+            created_at: time::get_time()
+        }
+    }).unwrap();
+    articles.map(|article| article.unwrap()).collect()
+}
+
+impl Service {
+    #[allow(dead_code)]
+    pub fn select(&self) -> Result<Vec<Article>> {
+        let mut stmt = self.c.prepare("SELECT id, title, desc, content FROM articles")?;
+        Ok(get_result(&mut stmt, &[]))
     }
+
+    pub fn insert(&self, object: &ToArticle) -> Result<i32> {
+        let mut stmt = self.c.prepare("INSERT INTO articles(title, desc, content) VALUES(?, ?, ?)")?;
+        let article = object.to_article();
+        let id = stmt.insert(&[&article.title, &article.desc, &article.content])? as i32;
+        Ok(id)
+    }
+
+    pub fn update(&self, id: i32, object: &ToArticle) -> Result<()> {
+        let mut stmt = self.c.prepare("UPDATE articles SET title = ?, desc = ?, content = ? WHERE id = ?")?;
+        let article = object.to_article();
+        stmt.execute(&[&article.title, &article.desc, &article.content, &id])?;
+        Ok(())
+    }
+
+    pub fn select_by_id(&self, id: i32) -> Result<Option<Article>> {
+        let mut stmt = self.c.prepare("SELECT id, title, desc, content FROM articles WHERE id = ?")?;
+        let v = get_result(&mut stmt, &[&id]);
+        let o = v.first();
+        Ok(Some(o.unwrap().clone()))
+    }
+}
+
+fn create(c: &Connection) -> Result<()> {
+    c.execute(
+        "CREATE TABLE articles(id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, title TEXT NOT NULL, desc TEXT NOT NULL, content TEXT NOT NULL)", &[])?;
+    Ok(())
+}
+
+fn insert(c: &Connection) -> Result<()> {
+    let mut stmt = c.prepare("INSERT INTO articles(title, desc, content) VALUES(?, ?, ?)")?;
+    for i in 0..100 {
+        stmt.execute(&[
+            &format!("Title_{:03}", i), &format!("Desc_{:03}", i), &format!("Content_{:03}", i)])?;
+    }
+    Ok(())
+}
+
+
+#[allow(unused_must_use)]
+pub fn init() -> Service {
+    let path = Path::new("sample.db");
+    fs::remove_file(path);
+    let c: Connection = Connection::open(path).unwrap();
+
+    create(&c).unwrap();
+    insert(&c).unwrap();
+    Service { c: c }
 }
--- rustlang/mini_restful_db/src/main.rs
+++ rustlang/mini_restful_db/src/main.rs
@@ -4,61 +4,76 @@
 extern crate rocket;
 extern crate serde_json;
 extern crate serde;
-#[macro_use] extern crate rocket_contrib;
-#[macro_use] extern crate serde_derive;
+extern crate time;
+#[macro_use]
+extern crate rocket_contrib;
+#[macro_use]
+extern crate serde_derive;
 
-#[cfg(test)] mod tests;
 mod db;
 
+use db::{Service, Article, ToArticle};
+
 use rocket_contrib::{JSON, Value};
-use rocket::State;
-use std::collections::HashMap;
+use rocket::{State};
+use rocket::config::{Config, Environment};
+use rocket::logger::LoggingLevel;
 use std::sync::Mutex;
 
 type ID = usize;
-type MessageMap = Mutex<HashMap<ID, String>>;
+type ServiceContext = Mutex<Service>;
 
 #[derive(Serialize, Deserialize, Debug)]
-struct Message {
+struct Payload {
     id: Option<ID>,
-    contents: String
+    title: String,
+    desc: String,
+    content: String
 }
 
-// TODO: This example can be improved by using `route` with multiple HTTP verbs.
-#[post("/<id>", format = "application/json", data = "<message>")]
-fn new(id: ID, message: JSON<Message>, context: State<MessageMap>) -> JSON<Value> {
-    let mut map = context.lock().expect("map lock.");
-    if map.contains_key(&id) {
-        JSON(json!({
-            "status": "error",
-            "reason": "ID exists. Try put."
-        }))
-    } else {
-        map.insert(id, message.0.contents);
-        JSON(json!({ "status": "ok" }))
+impl ToArticle for Payload {
+    fn to_article(&self) -> Article {
+        let id = if self.id.is_none() { 0i32 } else { self.id.unwrap() as i32 };
+        Article {
+            id: id,
+            title: self.title.clone(),
+            desc: self.desc.clone(),
+            content: self.content.clone(),
+            created_at: time::get_time()
+        }
     }
 }
 
-#[put("/<id>", format = "application/json", data = "<message>")]
-fn update(id: ID, message: JSON<Message>, context: State<MessageMap>) -> Option<JSON<Value>> {
-    let mut map = context.lock().unwrap();
-    if map.contains_key(&id) {
-        map.insert(id, message.0.contents);
-        Some(JSON(json!({ "status": "ok" })))
-    } else {
-        None
-    }
+#[post("/", format = "application/json", data = "<payload>")]
+fn new(payload: JSON<Payload>, context: State<ServiceContext>) -> JSON<Value> {
+    let ctx = context.lock().expect("map lock.");
+    let id = ctx.insert(&payload.into_inner()).unwrap();
+    JSON(json!({ "status": "ok", "id" : id}))
+}
+
+#[put("/<id>", format = "application/json", data = "<payload>")]
+fn update(id: ID, payload: JSON<Payload>, context: State<ServiceContext>) -> Option<JSON<Value>> {
+    let ctx = context.lock().unwrap();
+    ctx.update(id as i32, &payload.into_inner()).unwrap();
+    Some(JSON(json!({ "status": "ok" })))
 }
 
 #[get("/<id>", format = "application/json")]
-fn get(id: ID, context: State<MessageMap>) -> Option<JSON<Message>> {
-    let map = context.lock().unwrap();
-    map.get(&id).map(|contents| {
-        JSON(Message {
-            id: Some(id),
-            contents: contents.clone()
-        })
-    })
+fn get(id: ID, context: State<ServiceContext>) -> Option<JSON<Payload>> {
+    println!("get({})", id);
+    let ctx = context.lock().unwrap();
+    let result = ctx.select_by_id(id as i32).unwrap();
+    if result.is_some() {
+        let article = result.unwrap();
+        Some(JSON(Payload {
+            id: Some(article.id as usize),
+            title: article.title,
+            desc: article.desc,
+            content: article.content
+        }))
+    } else {
+        None
+    }
 }
 
 #[error(404)]
@@ -70,14 +85,38 @@ fn not_found() -> JSON<Value> {
 }
 
 fn rocket() -> rocket::Rocket {
-    db::init();
+    let service = db::init();
+    //    let config = Config::build(Environment::Staging)
+    //        .address("0.0.0.0")
+    //        .port(8000)
+    //        .finalize().unwrap();
+    //    rocket::Rocket::custom(config, true)
+    rocket::logger::init(LoggingLevel::Debug);
     rocket::ignite()
         .mount("/message", routes![new, update, get])
         .catch(errors![not_found])
-        .manage(Mutex::new(HashMap::<ID, String>::new()))
+        .manage(Mutex::new(service))
 }
 
 fn main() {
     rocket().launch();
+}
+
+//TODO: https://rocket.rs/guide/testing/
+#[cfg(test)]
+mod test {
+    use super::rocket;
+    use rocket::testing::MockRequest;
+    use rocket::http::{Status, Method};
+    use rocket::http::{ContentType, Header};
+
+    #[test]
+    fn get() {
+        let rocket = rocket::ignite().mount("/message", routes![super::get]);
+        let mut req = MockRequest::new(Method::Get, "/message/10");
+        req.add_header(Header::new("Accept-Type", "application/json"));
+        let mut response = req.dispatch_with(&rocket);
+        assert_eq!(response.status(), Status::Ok);
+    }
 }
 
