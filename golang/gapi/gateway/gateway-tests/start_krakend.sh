739637b08875d5bf1e08467967a5bcbb70deb667
pastor <viruszold@gmail.com>
2019-10-12 01:15:08 +0300

    KrakenD configuration template

--- golang/gapi/gateway/gateway-tests/start_krakend.sh
+++ golang/gapi/gateway/gateway-tests/start_krakend.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-export FC_ENABLE=1 \
+export FC_ENABLE=1
 export FC_SETTINGS="$PWD/config/settings"
 export FC_PARTIALS="$PWD/config/partials"
 export FC_TEMPLATES="$PWD/config/templates"
