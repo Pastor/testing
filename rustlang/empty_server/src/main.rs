fcb215f8aef36c362f0aafcd45b13e2c96e719fe
pastor <viruszold@gmail.com>
2020-01-17 17:45:55 +0300

    Включение подстановки URL для debug режима

--- rustlang/empty_server/src/main.rs
+++ rustlang/empty_server/src/main.rs
@@ -10,12 +10,12 @@ extern crate rocket;
 extern crate rocket_contrib;
 
 use std::{env, io, result};
-use reqwest::blocking::{Response};
+use reqwest::blocking::Response;
 use std::borrow::Borrow;
 use std::borrow::BorrowMut;
 use std::cell::RefCell;
 use std::collections::HashMap;
-use std::io::{Bytes, Error, ErrorKind, Result};
+use std::io::{Bytes, Error, ErrorKind, Result, Read};
 use std::sync::Mutex;
 
 use rocket::fairing::AdHoc;
@@ -28,17 +28,60 @@ use std::ffi::OsString;
 #[cfg(test)]
 mod tests;
 
+struct ProxyRead {
+    inner: Box<Response>
+}
+
+impl ProxyRead {
+    fn new(r: Response)  -> ProxyRead {
+        ProxyRead { inner: Box::new(r) }
+    }
+}
+
+impl Read for ProxyRead {
+    fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
+        self.inner.read(buf)
+    }
+}
+
+#[cfg(debug_assertions)]
+fn proxy_url() -> Option<String> {
+    match env::var_os("DOCSTORE_HOSTNAME") {
+        Some(token) => Some(token.into_string().unwrap()),
+        None => Some("http://ya.ru".to_string())
+    }
+}
+
+#[cfg(not(debug_assertions))]
+fn proxy_url() -> Option<String> {
+    match env::var_os("DOCSTORE_HOSTNAME") {
+        Some(ref token) => Some(token.into_string().unwrap()),
+        None => None
+    }
+}
+
 #[allow(non_snake_case)]
 #[get("/?<GUID>&<SIG>")]
-fn proxy(GUID: Option<String>, SIG: Option<String>) -> io::Result<Stream<DataStream>> {
-    let hostname = env::var_os("DOCSTORE_HOSTNAME").or(Some(OsString::from("ya.ru")));
-    if hostname.is_some() && GUID.is_some() {
-        let url = format!("{}/*/{}", hostname.unwrap().into_string().unwrap(), GUID.unwrap());
-        let rr = reqwest::blocking::get(url.as_str());
-
-//        Ok(Stream::from(rr.bytes_stream()))
-//    } else {
-//        Err(Error::new(ErrorKind::InvalidInput, "Hostname for docstore not defined"))
+fn proxy(GUID: Option<String>, SIG: Option<String>) -> io::Result<Stream<ProxyRead>> {
+    let url = proxy_url();
+    if url.is_some() && GUID.is_some() {
+        let url = format!("{}/*/{}", url.unwrap(), GUID.unwrap());
+        let resp = reqwest::blocking::get(url.as_str()).unwrap();
+        return if resp.status().is_success() {
+            #[cfg(feature = "enable_uncompressed")]
+                {
+                    let mut arch = zip::ZipArchive::new(resp).unwrap();
+                    for i in 0..arch.len() {
+                        let mut file = arch.by_index(i).unwrap();
+                        if !file.name().contains("sig") {
+                            return Err(Error::new(ErrorKind::InvalidData, "Find signature"));
+                        }
+                    }
+                }
+            return Ok(Stream::from(ProxyRead::new(resp)));
+        } else {
+            Err(Error::new(ErrorKind::InvalidData, "Can't fetch GUID"))
+        };
     }
     Err(Error::new(ErrorKind::InvalidInput, "Hostname for docstore not defined"))
 }
