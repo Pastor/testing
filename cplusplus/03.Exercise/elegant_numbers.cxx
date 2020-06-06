318da5059ca0eec2081c93ce70518b41c5aba7e3
pastor <viruszold@gmail.com>
2020-06-07 01:55:08 +0300

    KiCAD Z80 testing

--- cplusplus/03.Exercise/elegant_numbers.cxx
+++ cplusplus/03.Exercise/elegant_numbers.cxx
@@ -5,8 +5,14 @@
 #include <iostream>
 #include <set>
 
+/*
+ * Оптимизировать ввод данных, спрятать его в абстрактном классе
+ */
+
 struct NumberFunction {
-    typedef double _NumberType;
+    typedef int _NumberType;
+
+    virtual std::string name() const = 0;
 
     virtual bool has_range() const = 0;
 };
@@ -23,23 +29,103 @@ struct NumberOneFunction : public NumberFunction {
     bool has_range() const override {
         return false;
     }
+
     virtual void execute(std::ostream &output, const _NumberType &endRange) = 0;
 };
 
-struct PythagoreNumber: public NumberRangeFunction {
-    bool operator()() {
-        return true;
+struct PythagoreNumber : public NumberRangeFunction {
+    void execute(std::ostream &output, const _NumberType &beginRange, const _NumberType &endRange) override {
+        for (_NumberType x = beginRange; x < endRange; ++x) {
+            for (_NumberType y = beginRange; y < endRange; ++y) {
+                for (_NumberType z = beginRange; z < endRange; ++z) {
+                    if (std::pow(x, 2) + std::pow(y, 2) == std::pow(z, 2)) {
+                        output << "(" << x << ", " << y << ", " << z << ")" << std::endl;
+                    }
+                }
+            }
+        }
     }
 
-    void execute(std::ostream &output, const _NumberType &beginRange, const _NumberType &endRange) override {
-
+    std::string name() const override {
+        return "Pythagores";
     }
 };
 
+struct FibonacciNumber : public NumberOneFunction {
+    class Fibonacci {
+        std::ostream &_output;
+        int _i;
+
+        explicit Fibonacci(std::ostream &output)
+                : _output(output), _i(0) {}
+
+        _NumberType next(const _NumberType &n) {
+            if (_i > 0)
+                _output << ", ";
+            if (n == 1 || n == 2) {
+                _output << "1";
+                _i++;
+                return 1;
+            }
+            const auto &v = next(n - 1) + next(n - 2);
+            _output << v;
+            _i++;
+            return v;
+        }
+    };
+
+    std::string name() const override {
+        return "Fibonacci";
+    }
+
+    void execute(std::ostream &output, const _NumberType &endRange) override {
+        output << "(";
+        Fibonacci(output).next(endRange);
+        output << ")" << std::endl;
+    }
+};
+
+static std::vector<NumberFunction *> functions;
 
 int main(int argc, char **argv) {
-    auto p = new PythagoreNumber();
-    auto n = reinterpret_cast<NumberFunction *>(p);
-    auto d = reinterpret_cast<NumberRangeFunction *>(p);
+    std::ostream &output = std::cout;
+
+    functions.push_back(new PythagoreNumber());
+    functions.push_back(new FibonacciNumber());
+
+    while (true) {
+        auto select = 0;
+        for (auto i = 0; i < functions.size(); ++i) {
+            auto &func = functions.at(i);
+            std::cout << i << ". " << func->name() << " number." << std::endl;
+        }
+        std::cout << functions.size() << ". Exit" << std::endl;
+        std::cout << "Select action by number: ";
+        std::cin >> select;
+        if (select == functions.size())
+            break;
+        if (select >= 0 && select < functions.size()) {
+            auto &func = functions.at(select);
+            if (func->has_range()) {
+                NumberFunction::_NumberType beginRange;
+                NumberFunction::_NumberType endRange;
+
+                std::cout << "Input begin range: ";
+                std::cin >> beginRange;
+                std::cout << "Input end range: ";
+                std::cin >> endRange;
+                dynamic_cast<NumberRangeFunction *>(func)->execute(output, beginRange, endRange);
+            } else {
+                NumberFunction::_NumberType beginRange;
+
+                std::cout << "Input begin range: ";
+                std::cin >> beginRange;
+                dynamic_cast<NumberOneFunction *>(func)->execute(output, beginRange);
+            }
+        } else {
+            std::cerr << "Illegal select." << std::endl;
+            std::cerr.flush();
+        }
+    }
     return EXIT_SUCCESS;
 }
---
+++ kicad/Z80/Z80.sch
@@ -0,0 +1,27 @@
+EESchema Schematic File Version 4
+EELAYER 30 0
+EELAYER END
+$Descr A4 11693 8268
+encoding utf-8
+Sheet 1 1
+Title ""
+Date ""
+Rev ""
+Comp ""
+Comment1 ""
+Comment2 ""
+Comment3 ""
+Comment4 ""
+$EndDescr
+$Comp
+L CPU:Z80CPU U?
+U 1 1 5EDC1EEA
+P 5400 3500
+F 0 "U?" H 5400 5181 50  0000 C CNN
+F 1 "Z80CPU" H 5400 5090 50  0000 C CNN
+F 2 "" H 5400 3900 50  0001 C CNN
+F 3 "www.zilog.com/manage_directlink.php?filepath=docs/z80/um0080" H 5400 3900 50  0001 C CNN
+	1    5400 3500
+	1    0    0    -1  
+$EndComp
+$EndSCHEMATC
