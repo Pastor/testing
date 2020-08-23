880443b30f3c4c674217ae063047757146f48f60
pastor <viruszold@gmail.com>
2020-08-24 00:44:09 +0300

    notification continue. Part 4

--- python/notification_plugin/notification_delay.py
+++ python/notification_plugin/notification_delay.py
@@ -1,29 +1,45 @@
 #!/usr/bin/python
 # coding=utf-8
-import os
+import requests
+import datetime
+from filelock import FileLock
 from random import randrange
 from time import sleep
 
-fd = open('/var/opt/notification_delay.txt', 'r+')
-while True:
-    line = fd.readline()
-    if len(line) == 0:
-        break
-    line = line.strip()
-    if len(line) == 0:
-        continue
-    i = line.find(':')
-    if i > 0:
-        number = line[:i]
-        message = line[i + 1:]
-        # Случайной число в диапазоне 1, 2
-        delay = randrange(2, 4)
-        # Задержка в серкундах
-        sleep(delay)
-        command = "yowsup-cli demos --config-phone 74956642548 --send {0} \"{1}\" >> " \
-                  "/var/opt/notification_delay.log 2>&1".format(number, message)
-        os.system(command)
-        print 'Отсылка "%s" по номеру "%s"' % (message, number)
-fd.seek(0, 0)
-fd.truncate()
-fd.close()
+base_directory = '/var/opt/'
+base_directory = ""
+not_sent = []
+
+with FileLock(base_directory + 'notification_delay.lock'):
+    fd = open(base_directory + 'notification_delay.txt', 'r+')
+    while True:
+        line = fd.readline()
+        if len(line) == 0:
+            break
+        line = line.strip()
+        if len(line) == 0:
+            continue
+        i = line.find(':')
+        if i > 0:
+            number = line[:i]
+            message = line[i + 1:]
+            # Случайной число в диапазоне 1, 2
+            delay = randrange(1, 2)
+            # Задержка в серкундах
+            sleep(delay)
+            fdo = open(base_directory + 'notification_delay.log', 'a+')
+            try:
+                response = requests.post('http://127.0.0.1:8090/send', json={"to": number, "text": message})
+                fdo.write('[%s] %s: %s' % (datetime.datetime.now(), number, message))
+            except:
+                not_sent.append(line)
+                fdo.write('[%s] Ошибка отправки.  %s: %s' % (datetime.datetime.now(), number, message))
+            fdo.write("\n")
+            fdo.close()
+    fd.seek(0, 0)
+    fd.truncate()
+    fd.seek(0, 0)
+    for line in not_sent:
+        fd.write(line)
+        fd.write("\n")
+    fd.close()
