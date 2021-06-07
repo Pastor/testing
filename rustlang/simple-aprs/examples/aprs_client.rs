67b2b73a9d2f24b6a301dc5151322a95565784b1
Хлебников Андрей Александрович <Andrei.Khlebnikov@domrf.ru>
2021-06-07 20:11:28 +0300

    APRS simple

---
+++ rustlang/simple-aprs/examples/aprs_client.rs
@@ -0,0 +1,46 @@
+extern crate pretty_env_logger;
+
+use std::env;
+
+use simple_aprs::*;
+
+fn aprs_packet_handler(packet: APRSPacket) {
+    match packet.parsed() {
+        Ok(parsed) => {
+            println!("Source: {:#?}", parsed)
+        }
+        Err(err) => {
+            println!("Error parsing packet: {:?}", err);
+            match String::from_utf8(packet.raw) {
+                Ok(msg) => println!("{:?}", msg),
+                Err(err) => println!("Error converting APRS packet to UTF8: {}", err),
+            }
+        }
+    }
+}
+
+///http://www.aprs-is.net/javAPRSFilter.aspx
+///
+fn main() {
+    pretty_env_logger::init();
+
+    let args = arguments::parse(env::args()).unwrap();
+
+    let callsign = args.get::<String>("callsign").unwrap();
+    let passcode = args.get::<String>("passcode").unwrap();
+
+    let settings = ISSettings::new(
+        "euro.aprs2.net".to_string(),
+        14580,
+        callsign.to_string(),
+        passcode.to_string(),
+        "r/55/37/100".to_string(),
+    );
+
+    let aprs_is = IS::new(settings, aprs_packet_handler);
+
+    match aprs_is.connect() {
+        Ok(()) => println!("Disconnected"),
+        Err(err) => println!("An error occured: {}", err),
+    }
+}
---
+++ rustlang/simple-aprs/src/lib.rs
@@ -0,0 +1,142 @@
+extern crate aprs_parser;
+
+use std::error::Error;
+use std::time::Duration;
+
+use aprs_parser::{APRSError, APRSMessage};
+use futures::sink::SinkExt;
+use futures::StreamExt;
+use log::{info, trace, warn};
+use tokio::net::TcpStream;
+use tokio::time;
+use tokio_util::codec::{FramedRead, FramedWrite};
+
+mod codec;
+
+pub struct APRSPacket {
+    pub raw: Vec<u8>,
+}
+
+impl APRSPacket {
+    pub fn parsed(&self) -> Result<APRSMessage, APRSError> {
+        let str = String::from_utf8(self.raw.clone()).unwrap();
+        aprs_parser::parse(str.as_str())
+    }
+}
+
+pub struct ISSettings {
+    pub host: String,
+    pub port: u16,
+    pub callsign: String,
+    pub passcode: String,
+    pub filter: String,
+}
+
+impl ISSettings {
+    pub fn new(
+        host: String,
+        port: u16,
+        callsign: String,
+        passcode: String,
+        filter: String,
+    ) -> ISSettings {
+        ISSettings {
+            host,
+            port,
+            callsign,
+            passcode,
+            filter,
+        }
+    }
+}
+
+pub type PacketHandler = fn(APRSPacket);
+
+pub struct IS {
+    settings: ISSettings,
+    packet_handler: PacketHandler,
+}
+
+impl IS {
+    pub fn new(settings: ISSettings, packet_handler: PacketHandler) -> IS {
+        IS {
+            settings,
+            packet_handler,
+        }
+    }
+
+    #[tokio::main]
+    pub async fn connect(&self) -> Result<(), Box<dyn Error>> {
+        let address = format!("{}:{}", self.settings.host, self.settings.port);
+
+        let stream = TcpStream::connect(address).await?;
+
+        let (r, w) = stream.into_split();
+
+        let mut writer = FramedWrite::new(w, codec::ByteLinesCodec::new());
+        let mut reader = FramedRead::new(r, codec::ByteLinesCodec::new());
+
+        let login_message = {
+            let name = option_env!("CARGO_PKG_NAME").unwrap_or("unknown");
+            let version = option_env!("CARGO_PKG_VERSION").unwrap_or("0.0.0");
+
+            format!(
+                "user {} pass {} vers {} {}{}",
+                self.settings.callsign,
+                self.settings.passcode,
+                name,
+                version,
+                if self.settings.filter.is_empty() {
+                    "".to_string()
+                } else {
+                    format!(" filter {}", self.settings.filter)
+                }
+            )
+        };
+
+        info!("Logging on to APRS-IS server");
+        trace!("Login message: {}", login_message);
+        writer.send(login_message.as_bytes()).await?;
+
+        tokio::spawn(async move {
+            let mut interval = time::interval(Duration::from_secs(3600));
+            loop {
+                interval.tick().await;
+                info!("Sending keep alive message to APRS-IS server");
+                writer.send("# keep alive".as_bytes()).await.unwrap();
+            }
+        });
+
+        while let Some(packet) = reader.next().await {
+            match packet {
+                Ok(packet) => {
+                    if packet[0] == b'#' {
+                        match String::from_utf8(packet.to_vec()) {
+                            Ok(server_message) => {
+                                trace!("Received server response: {}", server_message);
+                                if server_message.contains("unverified") {
+                                    info!("User not verified on APRS-IS server");
+                                    continue;
+                                }
+                                if server_message.contains(" verified") {
+                                    info!("User verified on APRS-IS server");
+                                }
+                            }
+                            Err(err) => warn!("Error processing server response: {}", err),
+                        }
+                    } else {
+                        trace!("{:?}", packet);
+                        (self.packet_handler)(APRSPacket {
+                            raw: packet.to_vec(),
+                        });
+                    }
+                }
+                Err(err) => {
+                    warn!("Error processing packet from APRS-IS server: {}", err);
+                }
+            }
+        }
+
+        Ok(())
+    }
+}
